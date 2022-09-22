1c3c05e2113e760437207a467731e2a3b289a0ed
Heigon Alafaire Soldera Pires <hsoldera@corp.lifescan.com>
2022-09-22 08:48:43 -0300

    Revert "FPTI-19 Crud para filmes"
    This reverts commit e19ee85b5dda588ae88bda173ba629796c8f721a.

--- backend/backend_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from backend_api.models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
--- backend/backend_api/urls.py
+++
@@ -1,7 +0,0 @@
-from backend_api.views import MovieViewSet
-from rest_framework.routers import DefaultRouter
-from backend_api import views
-
-router = DefaultRouter()
-router.register(r'movies', views.MovieViewSet, basename='movie')
-urlpatterns = router.urls
