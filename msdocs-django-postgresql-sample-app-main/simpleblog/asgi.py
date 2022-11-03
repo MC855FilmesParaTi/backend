736ecfc94141279b080300ea603eb52378513978
heigon <h217638@dac.unicamp.br>
2022-11-03 05:12:23 -0300

    Sign up e login

--- msdocs-django-postgresql-sample-app-main/simpleblog/asgi.py
+++ msdocs-django-postgresql-sample-app-main/simpleblog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django-sql-project project.
+ASGI config for simpleblog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-sql-project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simpleblog.settings")
 
 application = get_asgi_application()
